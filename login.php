d950edb2771e34a6590202b55376dcbeb308acf1
Sreezon23 <158078280+Sreezon23@users.noreply.github.com>
2025-12-04 22:42:41 +0600

    Update login.php

--- login.php
+++ login.php
@@ -18,7 +18,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $messageType = $result['success'] ? 'success' : 'danger';
         
         if ($result['success']) {
-            header('Location: ' . SITE_URL . 'admin.php');
+            header('Location: admin.php');
             exit;
         }
     }
